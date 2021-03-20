38b1cc930bf3001abb1e3c1dd694e68cde69b9df
motoshifurugen <amanogawa_m@icloud.com>
2021-03-20 22:31:04 +0900

    CSSとJSを外部に切り出す

--- resources/views/download/index.blade.php
+++ resources/views/download/index.blade.php
@@ -1,47 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Top Page</title>
-    <style type="text/css">
-    .content {
-        text-align: center;
-        background-color: coral;
-        height: 680px;
-        padding: 20px;
-    }
-    .title {
-        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        margin: 2em auto;
-        font-size: 36px;
-        border-bottom: solid 10px #FFFFFF;
-        width: 50%;
-    }
-    .downloadBox {
-        border: solid 1px black;
-        background-color: cornflowerblue;
-        width: 20%;
-        margin: 2em auto;
-        padding: 1em;
-        border-radius: 1rem;
-    }
-    .downloadBox a{
-        text-decoration: none;
-        color: white;
-        font-size: 16px;
-    }
-    </style>
 </head>
 <body>
+    <div id="app">
+        <!-- デフォルトだとこの中ではvue.jsが有効 -->
+        <!-- example-component はLaravelに入っているサンプルのコンポーネント -->
+        {{-- <example-component></example-component> --}}
+    </div>
     <div class="content">
         <div class="title">
             <p>{{ $appName }}</p>
         </div>
         <div class="downloadBox">
-            <a href="#" download="sample.jpg">サンプル画像を保存</a>
-        </div>
-        <div class="downloadBox">
             <a href="/downloadFile">/downloadFile</a>
         </div>
     </div>
+<script src=" {{ mix('js/app.js') }} "></script>
 </body>
 </html>
