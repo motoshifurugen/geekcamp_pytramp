1bd931db26e434947f62c1537897bdbb272824cd
motoshifurugen <amanogawa_m@icloud.com>
2021-03-20 16:37:24 +0900

    CSS 直接記述

--- resources/views/download/index.blade.php
+++ resources/views/download/index.blade.php
@@ -2,7 +2,34 @@
 <html>
 <head>
     <title>Top Page</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <style type="text/css">
+    .content {
+        text-align: center;
+        background-color: coral;
+        height: 680px;
+        padding: 20px;
+    }
+    .title {
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        margin: 2em auto;
+        font-size: 36px;
+        border-bottom: solid 10px #FFFFFF;
+        width: 50%;
+    }
+    .downloadBox {
+        border: solid 1px black;
+        background-color: cornflowerblue;
+        width: 20%;
+        margin: 2em auto;
+        padding: 1em;
+        border-radius: 1rem;
+    }
+    .downloadBox a{
+        text-decoration: none;
+        color: white;
+        font-size: 16px;
+    }
+    </style>
 </head>
 <body>
     <div class="content">
