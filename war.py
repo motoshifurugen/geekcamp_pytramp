e58318f245026fd9cfef4d8df39ee40c73e8c5e8
鈴木 <suzukiharuya@MacBook-Pro-13.local>
2021-03-20 13:34:28 +0900

    removeCard作成

--- war.py
+++ war.py
@@ -3,6 +3,10 @@ class Deck:
         self.inHandCards = [None,None,2,3,4,5,6,7,8,9,10,11,12,13,14]
         self.usedCards = []
 
+    def removeCard(self,card):
+        self.inHandCards.remove(card)
+        self.usedCards.append(card)
+
 class Player:
     def __init__(self, name):
         self.name = name
