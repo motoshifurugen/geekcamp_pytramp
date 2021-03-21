c5363bb69a8512dd80df95c0ecfbaa4c0caebc8b
motoshifurugen <amanogawa_m@icloud.com>
2021-03-21 14:08:26 +0900

    Python環境構築ページ作成

--- resources/views/download/python.blade.php
+++ resources/views/download/python.blade.php
@@ -10,32 +10,45 @@
         <header-component></header-component>
         <div class="content-wrapper">
             <div class="content">
-                <div class="center-text">
+                <div class="page-center-text">
                     <div>
-                        <span class="title">Python実行環境構築</span>
-                    </div>
-                    <div class="sub-title">
-                        <span>Tramp Game Made In Python</span>
-                    </div>
-                    <div class="text-subtitle">2021.3.21 created by TrampM4kers</div>
-                    <div class="downloadBox">
-                        <a href="/downloadFile">PyTrampをダウンロードする</a>
-                    </div>
-                    <div class="button-box">
-                        <a class="button-wrapper" href="{{ url('/about') }}">
-                            <span class="button about"><span>PyTrampルール</span></span>
-                        </a>
-                        <a class="button-wrapper" href="{{ url('/python') }}">
-                            <span class="button about"><span>Python実行環境構築</span></span>
-                        </a>
-                        <a class="button-wrapper" href="{{ url('/trampmakers') }}">
-                            <span class="button about"><span>作成者紹介</span></span>
-                        </a>
+                        <span class="page-title">PyTrampをプレイするにはPython実行環境が必要です。</span>
                     </div>
                 </div>
+                <h5><a href="javascript:;" onclick="Display('no1')">Windows</a></h5>
+                <h5><a href="javascript:;" onclick="Display('no2')">Mac</a></h5>
+                <div class="card">
+                    <div class="card-body" id="SW1">
+                      <h5 class="card-title">Python環境構築手順（Windows）</h5>
+                      <p class="card-text"><span class="card-text-label">1. Pythonをインストール</span><br>Python公式サイト（https://www.python.org/）にアクセスし、「Download」からPythonをダウンロードします。</p>
+                      <p class="card-text"><span class="card-text-label">2. PyTramp（本アプリ）をダウンロード</span><br><a href="/zip-download">こちらのボタン</a>からPyTrampをダウンロードできます。（Zip形式）</p>
+                      <p class="card-text"><span class="card-text-label">3. Pythonを実行</span><br>分かりません分かりません。Pythonの実行方法が分かりません。</p>
+                    </div>
+                    <div class="card-body" id="SW2" style="display:none;">
+                        <h5 class="card-title">Python環境構築手順（Mac）</h5>
+                        <p class="card-text"><span class="card-text-label">1. Homebrewをインストール</span><br>ターミナルで下記を実行することでHomebrewをインストールします。<pre><code>/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"</code></pre></p>
+                        <p class="card-text"><span class="card-text-label">2. pyenvをインストール</span><br>Homebrewを使ってpyenvをインストールします。<pre><code>brew install pyenv</code></pre></p>
+                        <p class="card-text"><span class="card-text-label">3. pyenvの設定を行う</span><br>下記コマンドを順に実行することで、pyenvの設定を行います。<pre><code>echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile</code><code>echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile</code><code>echo 'eval "$(pyenv init -)"' >> ~/.bash_profile</code><code>source ~/.bash_profile</code></pre></p>
+                        <p class="card-text"><span class="card-text-label">4. Pythonをインストール</span><br>pyenvを使ってPythonをインストールします。（今回はv3.6.5）<pre><code>pyenv install 3.6.5</code></pre></p>
+                        <p class="card-text"><span class="card-text-label">5. PyTramp（本アプリ）をダウンロード</span><br><a href="/zip-download">こちらのボタン</a>からPyTrampをダウンロードできます。（Zip形式）</p>
+                      <p class="card-text"><span class="card-text-label">6. Pythonを実行</span><br>分かりません分かりません。Pythonの実行方法が分かりません。</p>
+                      </div>
+                      <a href="/" class="btn btn-primary back">Topへ戻る</a>
+                  </div>
             </div>
         </div>
     </div>
 <script src=" {{ mix('js/app.js') }} "></script>
+<script language="javascript" type="text/javascript">
+function Display(no){
+   if(no == "no1"){
+       document.getElementById("SW1").style.display = "block";
+       document.getElementById("SW2").style.display = "none";
+   }else if(no == "no2"){
+       document.getElementById("SW1").style.display = "none";
+       document.getElementById("SW2").style.display = "block";
+   }
+}
+</script>
 </body>
 </html>
