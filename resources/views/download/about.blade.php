13dc9315487a6f2253cc2693bd783f585b0f9d1d
motoshifurugen <amanogawa_m@icloud.com>
2021-03-21 11:09:42 +0900

    ルール説明ページ作成

--- resources/views/download/about.blade.php
+++ resources/views/download/about.blade.php
@@ -3,36 +3,29 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>About Us</title>
+    <title>About PyTramp</title>
 </head>
 <body>
     <div id="app">
         <header-component></header-component>
         <div class="content-wrapper">
             <div class="content">
-                <div class="center-text">
+                <div class="page-center-text">
                     <div>
-                        <span class="title">このゲームのルール</span>
-                    </div>
-                    <div class="sub-title">
-                        <span>Tramp Game Made In Python</span>
-                    </div>
-                    <div class="text-subtitle">2021.3.21 created by TrampM4kers</div>
-                    <div class="downloadBox">
-                        <a href="/downloadFile">PyTrampをダウンロードする</a>
-                    </div>
-                    <div class="button-box">
-                        <a class="button-wrapper" href="{{ url('/about') }}">
-                            <span class="button about"><span>PyTrampルール</span></span>
-                        </a>
-                        <a class="button-wrapper" href="{{ url('/python') }}">
-                            <span class="button about"><span>Python実行環境構築</span></span>
-                        </a>
-                        <a class="button-wrapper" href="{{ url('/trampmakers') }}">
-                            <span class="button about"><span>作成者紹介</span></span>
-                        </a>
+                        <span class="page-title">このゲームのルール</span>
                     </div>
                 </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">数字の大小で勝負するシンプルなゲーム</h5>
+                      <p class="card-text"><span class="card-text-label">■対戦人数</span><br>2人</p>
+                      <p class="card-text"><span class="card-text-label">■ゲームの開始</span><br>各プレイヤーが1~Kまでのカードを1枚ずつ合計13枚の手札を持ってスタートします。</p>
+                      <p class="card-text"><span class="card-text-label">■バトル</span><br>各プレイヤーが順番に1枚のカードを選びます。一度選んだカードは手札からなくなります。2人が選び終わるとそれぞれが選んだカードが表示されます。数字の大きかった方にWinがつきます。同じ数字の場合はDrawになります。</p>
+                      <p class="card-text"><span class="card-text-label">■最終結果</span><br>合計13回のバトルを終えて、Winの回数が多かったプレイヤーの勝利となります。</p>
+                      <p class="card-text"><span class="card-text-label">ゲームの面白さ</span><br>どの順番でカードを出すのか、相手に残っている手札はなんなのかを考えながらカードを選択するのが勝利への鍵となります。</p>
+                      <a href="/" class="btn btn-primary back">Topへ戻る</a>
+                    </div>
+                  </div>
             </div>
         </div>
     </div>
