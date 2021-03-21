ec9ee0597827855f1c7ab43d1a20043760b52892
M4SHR0 <fall.est.mori@gmail.com>
2021-03-21 11:46:31 +0900

    ボタン調整
    ボタンの調整

--- main_window.py
+++ main_window.py
@@ -18,48 +18,49 @@ class GameWindow(tkinter.Frame):
         self.default_font=tkinter.font.Font(family="System",size=48)
         self.button_font=tkinter.font.Font(family="System",size=36)
         self.label_font=tkinter.font.Font(family="System",size=24)
+        """
         #現在のゲーム数
         game_count_label=tkinter.Label(self,
             text="{}ゲーム目".format(self.game_count()),font=(self.label_font))
         game_count_label.grid(row=0,column=0,sticky=tkinter.NW)
-        
+        """
         #相手プレイヤーのデッキ
         player2_deck_list=[]
         for _ in range(14):
             player2_deck_list.append(tkinter.Button(self,text="{}".format(_),
-                font=(self.label_font) ,command=lambda:self.callback(_)))
-            player2_deck_list[_].grid(row=1,column=_,padx=3,pady=10)
+                font=(self.label_font),width=2 ,command=lambda:self.callback(_)))
+            player2_deck_list[_].grid(row=2,column=_,padx=1,pady=10)
 
-        """ここから1行に含める"""
+        """ここから1行に含める
         #相手の獲得ゲーム数
         player2_game_count_label=tkinter.Label(self,
             text="勝利数 {}".format(self.player2_game_count()),font=(self.label_font))
-        player2_game_count_label.grid(row=2,column=1,sticky=tkinter.NW)
+        player2_game_count_label.grid(row=4,column=1,sticky=tkinter.NW)
         
         #相手のカードの画像表示
         player2_card=tkinter.PhotoImage(file="img\sample1.png")
         canvas=tkinter.Canvas(bg="white",width=100,height=200)
         canvas.pack() 
-        """pack()でないとcanvasは配置できない?1行に収められるように工夫が必要"""
+        #pack()でないとcanvasは配置できない?1行に収められるように工夫が必要
         canvas.create_image(0,0,image=player2_card)
         #自分のカードの画像表示
         player1_card=tkinter.PhotoImage(file="img\sample{}.png".format(1))
         canvas=tkinter.Canvas(bg="white",width=100,height=200)
         canvas.pack()
-        """pack()でないとcanvasは配置できない?1行に収められるように工夫が必要"""
+        #pack()でないとcanvasは配置できない?1行に収められるように工夫が必要
         canvas.create_image(0,0,image=player1_card)
         #自分の獲得ゲーム数
         player1_game_count_label=tkinter.Label(self,
             text="勝利数 {}".format(self.player2_game_count()),font=(self.label_font))
-        player1_game_count_label.grid(row=2,column=4,sticky=tkinter.SE)
-        """ここまで1行に含める"""
+        player1_game_count_label.grid(row=6,column=4,sticky=tkinter.SE)
+        ここまで1行に含める"""
         
         #自分のデッキ
         player1_deck_list=[]
         for _ in range(14):
             player1_deck_list.append(tkinter.Button(self,text="{}".format(_),
-                font=(self.label_font) ,command=lambda:self.callback(_)))
-            player1_deck_list[_].grid(row=3,column=_,padx=3,pady=10)
+                font=(self.label_font),width=2 ,command=lambda:self.callback(_)))
+            player1_deck_list[_].grid(row=3,column=_,padx=1,pady=10)
 
     #テスト用
     def game_count(self):
