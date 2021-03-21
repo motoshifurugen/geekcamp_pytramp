56ee4c531b20f7041c90602420a195a8e0a23760
M4SHR0 <fall.est.mori@gmail.com>
2021-03-21 09:07:28 +0900

    プレイヤー2のデッキボタン作成
    プレイヤー2のデッキボタン作成とそのコールバック関数のテスト

--- main_window.py
+++ main_window.py
@@ -2,20 +2,51 @@ import tkinter
 from tkinter.font import Font
 
 class GameWindow(tkinter.Frame):
-    def __init__(self,main = None):
-        super().__init__(main)
-        main.title("PyTrump")
-        main.geometry("900x700")
-        main.resizable(False,False)
+    def __init__(self,master = None):
+        tkinter.Frame.__init__(self,master)
+        self.master.title("PyTrump")
+        master.geometry("900x700")
+        master.resizable(False,False)
+        
+        self.pack()
+        self.create_widget()
+
+    def create_widget(self):
+        #フォント
         self.default_font=tkinter.font.Font(family="System",size=48)
         self.button_font=tkinter.font.Font(family="System",size=36)
-        self.game_count_label=tkinter.Label(main,
-            text="{}ゲーム目".format(game_count),font=(self.default_font))
-        self.game_count_label.grid(row=0,column=0)
+        self.label_font=tkinter.font.Font(family="System",size=24)
+        #現在のゲーム数
+        game_count_label=tkinter.Label(self,
+            text="{}ゲーム目".format(self.game_count()),font=(self.label_font))
+        game_count_label.grid(row=0,column=0,sticky=tkinter.NW)
+        
+        #相手プレイヤーのデッキ
+        player2_deck_list=[]
+        for _ in range(14):
+            player2_deck_list.append(tkinter.Button(self,text="{}".format(_),
+                font=(self.label_font) ,command=lambda:self.callback(_)))
+            player2_deck_list[_].grid(row=1,column=_,padx=3,pady=10)
 
-    
+        """ここから1行に含める"""
+        #相手の獲得ゲーム数
+        #相手のカード
+        #自分のカード
+        #自分の獲得ゲーム数
+        """ここまで1行に含める"""
+        
+        #自分のデッキ
 
+
+    #テスト用
+    def game_count(self):
+        GAME_COUNT=10
+        return GAME_COUNT
+    #テスト用
+    def callback(self,x):
+        print("ボタン{}".format(x))
+        
 #テスト用
 window=tkinter.Tk()
-test=GameWindow(main=window)
+test=GameWindow(window)
 test.mainloop()
